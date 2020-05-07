c6bd0278ca222712925040769501df289c1aa26c
Lsansaloni <ll.sansaloni@gmail.com>
2020-05-07 21:52:57 +0200

    Part 2
    ALl prototyping in unity tutorial part 2

--- Assets/Scripts/MouseManage.cs
+++ Assets/Scripts/MouseManage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManage : MonoBehaviour
 {
@@ -12,26 +13,68 @@ public class MouseManage : MonoBehaviour
     public Texture2D doorway;//cursor for doorways
     public Texture2D combat;//cursor combat action
 
+    public EventVector3 OnclickEnviroment;
 
-  
+
+
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
         {
             bool door = false;
-            if(hit.collider.gameObject.tag == "Doorway"){
+            bool item = false;
+
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
                 Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }           
+            else if(hit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combat, new Vector2(16, 16), CursorMode.Auto);
+                item = true;
             }
             else
             {
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+                
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (door)
+                {
+                    Transform doorway = hit.collider.gameObject.transform;
+
+                    OnclickEnviroment.Invoke(doorway.position);
+                    Debug.Log("DOOOR");
+                }
+                else if(item)
+                {
+                    Transform itemPos = hit.collider.gameObject.transform;
+
+                    OnclickEnviroment.Invoke(itemPos.position);
+                    Debug.Log("ITEM");
+                }
+                else
+                {
+                    OnclickEnviroment.Invoke(hit.point);
+                }
             }
         }
         else
         {
+
             Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
         }
     }
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3>
+{
+
 }
