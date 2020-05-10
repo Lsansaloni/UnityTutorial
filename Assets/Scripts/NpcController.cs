fe6bfb4605ee24425b98e15f46c2df910f23e602
Lsansaloni <ll.sansaloni@gmail.com>
2020-05-10 12:59:01 +0200

    NPC Controler

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcControler : MonoBehaviour
+{
+    public float patrolTime = 10f;
+    public float aggroRange = 10f;
+    public Transform[] waypoints;
+
+    private int index;
+    private float speed, agentspeed;
+    private Transform player;
+
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+      //  anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        if( agent != null)
+        {
+            agentspeed = agent.speed;
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
+
+        InvokeRepeating("Tick", 0, 0.5f);
+        if(waypoints.Length > 0)
+        {
+            InvokeRepeating("Patrol", 0, patrolTime);
+        }
+    }
+
+    void Patrol()
+    {
+        index = index == waypoints.Length - 1 ? 0 : index + 1;
+    }
+    void Tick()
+    {
+        agent.destination = waypoints[index].position;
+    }
+}
