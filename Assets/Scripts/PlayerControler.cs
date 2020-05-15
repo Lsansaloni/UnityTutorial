d5d8a0231890268eef2a372eb5f2bd45e98216ea
Lsansaloni <ll.sansaloni@gmail.com>
2020-05-15 19:55:05 +0200

    hero speed animation

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,18 +5,18 @@ using UnityEngine.AI;
 
 public class PlayerControler : MonoBehaviour
 {
-    //private Animator anim;
+    private Animator anim;
     private NavMeshAgent agent;
     // Start is called before the first frame update
     void Awake()
     {
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetFloat("Speed", agent.velocity.magnitude);
     }
 }
